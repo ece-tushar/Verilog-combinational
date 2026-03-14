af94d0e14f120cc593a989dc171ef2e8e0eb149c
ece-tushar <indigotushar@gmail.com>
2026-03-14 15:23:06 +0530

    Update full_adder.v

--- full_adder/structural/full_adder.v
+++ full_adder/structural/full_adder.v
@@ -10,7 +10,7 @@ module FA_1bit(
     input a,b,cin,
     output sum, carry
     );
-    wire s1,c1, c2; //Intermediate vires for half adder outputs
+    wire s1,c1, c2; //Intermediate wires for half adder outputs
     half_adder HA1 (a,b,s1,c1); //Instantiate the first half adder
     half_adder HA2(s1,cin, sum,c2); //Instantiate the second half adder
     or o1 (carry,c1,c2); //assign carry= c1 | c2;
